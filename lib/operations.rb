52c36c1b2ee0a8a031c887ed6c6bf6eb5d7fe5df
Meagan Utsey <meagan.utsey@gmail.com>
2019-10-21 19:22:15 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
-def unsafe?(speed)
+def unsafe?("35")
+
+unsafe < 40 ? "Unsafe" : Safe 
 
 end
 
 
 
-def not_safe?(speed)
-	
+def not_safe?("95")
+
+not_safe > 60 ? "not_safe" : Safe 	
 end
-	
-
-
