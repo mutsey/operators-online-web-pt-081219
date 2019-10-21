dfe975c75e461a7a98327ca66cadaa5eba4ec63c
Meagan Utsey <meagan.utsey@gmail.com>
2019-10-21 19:21:52 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,12 @@
-def unsafe?("40-60")
+def unsafe?("35")
+
+unsafe < 40 ? "Unsafe" : Safe 
 
 end
 
 
 
-def not_safe?("<40",">60")
-	
+def not_safe?("95")
+
+not_safe > 60 ? "not_safe" : Safe 	
 end
-	
-
-
